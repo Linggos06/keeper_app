0628a5119087af46e2867064e05f55e1bb1ece32
Linggos06 <julia.snk@hotmail.com>
2021-11-02 01:10:54 +0300

    Updates

---
+++ precache-manifest.2668834024cc6b5753c466f5322c6319.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e2d989fe60f2e3f654c34a273f7676",
+    "url": "/keeper_app/index.html"
+  },
+  {
+    "revision": "bb903f9c8a9d913c693d",
+    "url": "/keeper_app/static/js/2.cd23cbdf.chunk.js"
+  },
+  {
+    "revision": "1e8113e785e5f6f151e2",
+    "url": "/keeper_app/static/js/main.64cf5fca.chunk.js"
+  },
+  {
+    "revision": "e04e243d96ad15f889d6",
+    "url": "/keeper_app/static/js/runtime-main.9bc4e032.js"
+  }
+]);
